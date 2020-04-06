fd4daa8982f92fce75e86c1e32c90f4ed1dcef67
mjok <jokubauskas@gmail.com>
2020-04-06 19:32:29 +0300

    version change

--- newModule.bat
+++ newModule.bat
@@ -2,4 +2,4 @@ mvn archetype:generate -DarchetypeGroupId=com.jkoolcloud.remora^
                        -DarchetypeArtifactId=remora-archetype^
                        -DarchetypeVersion=1.0-SNAPSHOT^
                        -DgroupId=com.jkoolcloud.remora^
-                       -Dversion=0.1.5-SNAPSHOT
+                       -Dversion=0.1.6-SNAPSHOT
