74708958f813dbe13bd0a9d372468af875f38563
mjok <jokubauskas@gmail.com>
2020-04-30 09:18:35 +0300

    Update copyright

--- remora-stream/src/test/java/com/jkoolcoud/remora/ChronicleQueueTest.java
+++ remora-stream/src/test/java/com/jkoolcoud/remora/ChronicleQueueTest.java
@@ -1,21 +1,17 @@
 /*
+ * Copyright 2019-2020 NASTEL TECHNOLOGIES, INC.
  *
- * Copyright (c) 2019-2020 NasTel Technologies, Inc. All Rights Reserved.
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
  *
- * This software is the confidential and proprietary information of NasTel
- * Technologies, Inc. ("Confidential Information").  You shall not disclose
- * such Confidential Information and shall use it only in accordance with
- * the terms of the license agreement you entered into with NasTel
- * Technologies.
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
- * NASTEL MAKES NO REPRESENTATIONS OR WARRANTIES ABOUT THE SUITABILITY OF
- * THE SOFTWARE, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
- * PURPOSE, OR NON-INFRINGEMENT. NASTEL SHALL NOT BE LIABLE FOR ANY DAMAGES
- * SUFFERED BY LICENSEE AS A RESULT OF USING, MODIFYING OR DISTRIBUTING
- * THIS SOFTWARE OR ITS DERIVATIVES.
- *
- * CopyrightVersion 1.0
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
 
 package com.jkoolcoud.remora;
