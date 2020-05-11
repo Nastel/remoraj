b263dd88e89b4e5996f20adfe2e2736ba3962f70
mjok <jokubauskas@gmail.com>
2020-05-11 16:43:59 +0300

    socket advice

---
+++ remora-socket/src/test/java/com/jkoolcloud/remora/advices/SocketAdviceTest.java
@@ -0,0 +1,26 @@
+package com.jkoolcloud.remora.advices;
+
+import org.junit.Test;
+
+//Enable power mockito if any of classes failing to mock
+//@RunWith(PowerMockRunner.class)
+//@PrepareForTest({WebApp.class})
+//@SuppressStaticInitializationFor({""})
+public class SocketAdviceTest {
+
+	@Test
+	public void testSocketAdviceInterceptor() throws NoSuchMethodException {
+		// PowerMockito.mockStatic(<<classToIntercept>>.class);
+		// WebApp webApp=mock(<<classToIntercept>>.class);
+
+		// EntryDefinition handleRequestEntry = new EntryDefinition(SocketAdvice.class);
+
+		// Method method=Whitebox.getMethod(Object.class,"<<interceptingMethod>>");
+
+		// test before method
+		// SocketAdviceAdvice.before();
+
+		// test after method
+		// SocketAdviceAdvice.after();
+	}
+}
