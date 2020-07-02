9b29c6750143bc1d495caae46c1865ae1b775463
mjok <jokubauskas@gmail.com>
2020-07-02 16:21:10 +0300

    reading token from file

--- bin/run.bat
+++ bin/run.bat
@@ -1,2 +1,16 @@
-cd tnt4j-streams\remora-streamer
-start run.bat
+@echo off
+:START
+IF EXIST token (
+	echo "Reading token from file"
+	set /P STREAMING_TOKEN=<token
+	echo "TOKEN: %STREAMING_TOKEN%"
+) ELSE (
+	echo "Token not found"
+	set /P STREAMING_TOKEN=Enter Your Streaming token:
+	@echo on
+	echo %STREAMING_TOKEN% > token
+	@echo off
+	goto START
+)
+rem cd tnt4j-streams\remora-streamer
+rem start run.bat
--- bin/run.sh
+++ bin/run.sh
@@ -1,3 +1,13 @@
 #! /bin/bash
+if [ -e token ] 
+then
+	echo "Reading token from file"
+	export STREAMING_TOKEN=<token
+	echo "TOKEN: %STREAMING_TOKEN%"
+else
+	echo "Token not found"
+	read -p "Enter Your Streaming token:" STREAMING_TOKEN
+	echo %STREAMING_TOKEN% > token
+fi	
 cd tnt4j-streams/remora-streamer
 ./run.sh
