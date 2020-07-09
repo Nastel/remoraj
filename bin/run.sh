916cc9fa9063f3916f9928c14104f6d8d04c356c
mjok <jokubauskas@gmail.com>
2020-07-09 11:40:42 +0300

    reading token from file fix: *nix

--- bin/run.sh
+++ bin/run.sh
@@ -2,13 +2,13 @@
 if [ -e token ] 
 then
 	echo "Reading token from file"
-	export STREAMING_TOKEN=<token
-	echo "TOKEN: %STREAMING_TOKEN%"
+	export STREAMING_TOKEN=$(<token)
+	echo "TOKEN: $STREAMING_TOKEN"
 else
 	echo "Token not found"
 	read -p "Enter Your Streaming token:" STREAMING_TOKEN
-	echo %STREAMING_TOKEN% > token
+	echo $STREAMING_TOKEN > token
 fi	
-export STREAMSOPTS=%STREAMSOPTS% -DSTREAMING_TOKEN=%STREAMING_TOKEN%
+export STREAMSOPTS="$STREAMSOPTS -DSTREAMING_TOKEN=$STREAMING_TOKEN"
 cd tnt4j-streams/remora-streamer
 ./run.sh
